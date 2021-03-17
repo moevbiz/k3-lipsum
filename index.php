f0d977eaa164a3f3e84985a669c078773cdfea2e
Bruno <38008341+moevbiz@users.noreply.github.com>
2021-03-17 19:51:43 +0100

    "k3-"

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-Kirby::plugin('moevbiz/lipsum', [
+Kirby::plugin('moevbiz/k3-lipsum', [
     'pageMethods' => [
         'lipsum' => function () {
             return new joshtronic\LoremIpsum();
